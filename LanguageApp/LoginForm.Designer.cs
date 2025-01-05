9bc475dd9979c48d777f742a31357c2fb349c89a
jihan0-0 <jihanmutia76@gmail.com>
2025-01-05 13:07:02 +0700

    Add project files.

---
+++ LanguageApp/LoginForm.Designer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace LanguageApp
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=localhost;Initial Catalog=MultiLanguageDB;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Username", txtUsername.Text);
+                        command.Parameters.AddWithValue("@Password", txtPassword.Text);
+                        int userCount = (int)command.ExecuteScalar();
+                        if (userCount > 0)
+                        {
+                            // Login berhasil, buka form utama
+                            MainForm mainForm = new MainForm();
+                            mainForm.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Username atau Password salah.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Terjadi kesalahan: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+            // Buka form pembuatan akun baru
+            CreateAccountForm createAccountForm = new CreateAccountForm();
+            createAccountForm.Show();
+            this.Hide();
+        }
+    }
+}
